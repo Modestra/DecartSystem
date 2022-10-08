06ac87fe99bdfaa01baa18043e7b147f5b4e3bd6
Kirill <104451159+Modestra@users.noreply.github.com>
2022-10-08 14:06:54 +0700

    Start Position

---
+++ Алгоритмы/Алгоритмы/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Алгоритмы
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.inputName = new System.Windows.Forms.Label();
+            this.decartSpace = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.decartSpace)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(127, 28);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 99);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(285, 27);
+            this.textBox1.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(12, 132);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(285, 304);
+            this.listBox1.TabIndex = 2;
+            // 
+            // inputName
+            // 
+            this.inputName.AutoSize = true;
+            this.inputName.Location = new System.Drawing.Point(12, 76);
+            this.inputName.Name = "inputName";
+            this.inputName.Size = new System.Drawing.Size(72, 20);
+            this.inputName.TabIndex = 3;
+            this.inputName.Text = "Формула";
+            // 
+            // decartSpace
+            // 
+            this.decartSpace.Location = new System.Drawing.Point(303, 12);
+            this.decartSpace.Name = "decartSpace";
+            this.decartSpace.Size = new System.Drawing.Size(494, 426);
+            this.decartSpace.TabIndex = 4;
+            this.decartSpace.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.decartSpace);
+            this.Controls.Add(this.inputName);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.decartSpace)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ComboBox comboBox1;
+        private TextBox textBox1;
+        private ListBox listBox1;
+        private Label inputName;
+        private PictureBox decartSpace;
+    }
+}
